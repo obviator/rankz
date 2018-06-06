e3f2b4a7848cfde5c5411ba1de646b278a9ba70c
Tom <github.e22@neverbox.com>
2018-06-06 15:26:44 +0100

    reorganised routes so that form_with(tournament) would select the correct path. Fixes #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :tournaments, path: '', only: %i[index]
-
   resources :tournaments, path: 't', except: %i[index] do
     resources :teams, shallow: true
   end
 
+  resources :tournaments, path: '', only: %i[index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tournaments#index'
 end
