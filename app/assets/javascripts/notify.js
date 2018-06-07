976de4727f5a71af2ff08a82d09f663bde9336ac
Tom <github.e22@neverbox.com>
2018-06-07 16:08:38 +0100

    Moved :notice messages to js notifications

--- app/assets/javascripts/notify.js
+++ app/assets/javascripts/notify.js
@@ -12,7 +12,8 @@ $.notifyDefaults({
         enter: "animated fadeInRight",
         exit: "animated fadeOutDown"
     },
-    template: '<div data-notify="container" class="col-xs-11 col-sm-1 alert alert-{0}" role="alert">' +
+    template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert' +
+    ' alert-{0}" role="alert">' +
     '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
     '<span data-notify="icon"></span> ' +
     '<span data-notify="title">{1}</span> ' +
