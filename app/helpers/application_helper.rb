a3f5be2326f16ee3c6efbf5a17d33cecc39da9c5
Tom <github.e22@neverbox.com>
2018-07-20 12:24:10 +0100

    Load controller specific JS as part of Application.js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,10 +41,10 @@ module ApplicationHelper
     adorable_url = "https://api.adorable.io/avatars/#{adorable_size}/#{adorable_id}.png"
     image_tag(adorable_url, alt: user.username, class: "img-circle elevation-2")
   end
-
-  def controller_javascript(opts = {})
-    if Rails.application.assets.find_asset("controllers/#{controller_name}.js")
-      javascript_include_tag("controllers/#{controller_name}", opts)
-    end
-  end
+  #
+  # def controller_javascript(opts = {})
+  #   if Rails.application.assets.find_asset("controllers/#{controller_name}.js")
+  #     javascript_include_tag("controllers/#{controller_name}", opts)
+  #   end
+  # end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,11 +8,11 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Add controller specific js to asset load path
-Rails.application.config.assets.paths << Rails.root.join('assets/controllers')
+# Rails.application.config.assets.paths << Rails.root.join('assets/controllers')
 
 # Precompile additional assets.
 # application.js, application.css.sass, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 # Add controller specific js to asset precompile path
-Rails.application.config.assets.precompile += ['javascripts/controllers/*']
+# Rails.application.config.assets.precompile += ['javascripts/controllers/*']
